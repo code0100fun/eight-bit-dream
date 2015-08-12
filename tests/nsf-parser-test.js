df396690f179fdeee2307a5665c887e97718de3d
Chase McCarthy <chase@code0100fun.com>
2015-08-12 00:49:42 -0400

    Add mocha tests

---
+++ tests/nsf-parser-test.js
@@ -0,0 +1,10 @@
+import hello from '../eight-bit-dream/nsf-parser';
+import { expect } from 'chai';
+
+describe('NSF Parser', function(){
+
+  it('example', function(){
+    expect(hello()).to.equal('hello world');
+  });
+
+});
